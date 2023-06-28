83c7b0b4fce2107d4eb4e66657032ccf72d35086
Lucas de Lacerda <lucasdelacerdamp@gmail.com>
2023-06-28 10:40:41 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -23,13 +23,13 @@ function menuRelatorioInner()
     (function
         (chave, pos)
     {
-        let pessoa = JSON.parse(localStorage.getItem(chave));
+        let usuario = JSON.parse(localStorage.getItem(chave));
         html = html+"<tr>";
-        html=html+"<td><a href=\"#\" onclick=\"buscaCPF('" +  pessoa.cpf + "');\">"+pessoa.cpf+"</a></td>"; //Adiciona a célula (TD) com o CPF da pessoa. Também é inserido o evento onClick para permitir a edição do cadastro
-        html=html+"<td>"+pessoa.nome+"</td>"; //Adiciona a célula de dados (TD) com o nome da pessoa
-        html=html+"<td>"+pessoa.email+"</td>"; //Adiciona a célula de dados (TD) com o e-mail da pessoa
-        html=html+"<td>"+pessoa.nasc+"</td>"; //Adiciona a célula de dados (TD) com o a data de nascimento da pessoa
-        html=html+"</tr>"; //Encerra o elemento TR (linha) da pessoa em processamento
+        html=html+"<td><a href=\"#\" onclick=\"Mensagem('" +  usuario.mensagem + "');\">"+usuario.mensagem+"</a></td>";
+        html=html+"<td>"+usuario.email+"</td>";
+        html=html+"<td>"+usuario.pnome+"</td>";
+        html=html+"<td>"+usuario.snome+"</td>";
+        html=html+"</tr>";
     }
     );
     document.getElementById("corpoTabela").innerHTML=html; //Busca o elemento do corpo da tabela e altera o conteúdo para o trecho de HTML gerado no laço (repetição) acima
