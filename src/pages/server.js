50f087b82d15d4f128c2fbdca061e0b67810f510
Lucas de Lacerda <lucasdelacerdamp@gmail.com>
2023-06-28 22:50:01 -0300

    Update server.js

--- src/pages/server.js
+++ src/pages/server.js
@@ -1,35 +1,18 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// Rota para receber os dados do formulário
-app.post('/src/pages/server.js', (req, res) => {
-  let rawData = '';
+app.use(express.json());
+
+app.post('formulario', (req, res) => {
+  console.log('Recebido:', req.body);
   
-  req.on('data', (chunk) => {
-    rawData += chunk;
-  });
+  // Código para armazenar os dados em um arquivo JSON
+  // ...
   
-  req.on('end', () => {
-    try {
-      const jsonData = JSON.parse(rawData);
-      
-      // Armazene o JSON em um arquivo
-      fs.writeFile('src/pages/db.json', JSON.stringify(jsonData), (err) => {
-        if (err) throw err;
-        console.log('Dados do formulário armazenados com sucesso!');
-      });
-      
-      res.sendStatus(200);
-    } catch (error) {
-      console.error('Erro ao armazenar os dados do formulário:', error);
-      res.sendStatus(500);
-    }
-  });
+  res.sendStatus(200);
 });
 
-// Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor Node.js em execução na porta ${port}`);
 });
